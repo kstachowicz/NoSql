3683e4f55001f1876ea23ac97f4b98ecce4b2a32
Kamil <kamil@kamil-VirtualBox.(none)>
2012-05-13 20:34:57 +0200

    added menu in converters script

--- json2couchdb.sh
+++ json2couchdb.sh
@@ -1,4 +1,58 @@
+#!/bin/bash
+#import technology diggs from digg.com to couchdb
+
+db=$1;
+
+
+clear
+
+echo "          json2couchdb script"
+echo "          ------- ----"
+echo "Choose one of the following options (press the letter):" 
+echo
+echo "[H]elp, for information how to use the script"
+echo "[C]onvert json data to couchdb (DB:" $db ")"
+echo "[Q]uit"
+echo
+
+read option
+
+case "$option" in
+
+
+  "H" | "h" )
+  echo
+  echo "You pressed [H]. That means, you want to know more about using the script"
+  echo "Run file: ./json2couchdb.sh [NAME_OF_THE_DATABASE]"
+  echo "We assume you are working on the localhost database. Please edit the script"
+  echo "if you are using another host"
+  ;;
+
+   "C" | "c" )
+  echo
+  echo "You pressed [C]. That means, you want to convert you json data to mongodb"
+  echo "You run file with parameters:"
+  echo "NAME OF THE DATABASE: " $db
+
+ if [ $# -ne 1 ]; then
+	echo "Invalid start of the file. Please check [H]elp option for more details";
+
+else
+echo "Deleting the database: $dbName "
+curl -X DELETE  "http://localhost:5984/"$db
+
+echo "Creating new database: $dbName "
+curl -X PUT  "http://localhost:5984/"$db
+
 while read line 
 do
-	curl -d "$line" -X POST -H "Content-Type: application/json" "http://localhost:5984/lz/" #$host/$dbName
+	curl -d "$line" -X POST -H "Content-Type: application/json" "http://localhost:5984/"$db #$host/$dbName
 done < "technology.json";
+
+fi
+;;
+esac
+
+exit 0
+
+
--- json2mongodb.sh
+++ json2mongodb.sh
@@ -1,4 +1,47 @@
 #!/bin/bash
 #import technology diggs from digg.com to mongodb
 
-mongoimport --host localhost --db test --collection diggs --type json --file technology.json
+db=$1;
+collection=$2;
+
+
+clear
+
+echo "          json2mongodb script"
+echo "          ------- ----"
+echo "Choose one of the following options (press the letter):" 
+echo
+echo "[H]elp, for information how to use the script"
+echo "[C]onvert json data to mongodb (DB:" $db " COLLECTION:" $collection ")"
+echo "[Q]uit"
+echo
+
+read option
+
+case "$option" in
+
+
+  "H" | "h" )
+  echo
+  echo "You pressed [H]. That means, you want to know more about using the script"
+  echo "Run file: ./json2mongodb.sh [NAME_OF_THE_DATABASE] [NAME_OF_THE_COLLECTION]"
+  echo "We assume you are working on the localhost database. Please edit the script"
+  echo "if you are using another host"
+  ;;
+
+   "C" | "c" )
+  echo
+  echo "You pressed [C]. That means, you want to convert you json data to mongodb"
+  echo "You run file with parameters:"
+  echo "NAME OF THE DATABASE: " $db
+  echo "NAME OF THE COLLECTION " $collection
+ if [ $# -ne 2 ]; then
+	echo "Invalid start of the file. Please check [H]elp option for more details";
+
+else
+mongoimport --host localhost --db $db --collection $collection --type json --file technology.json
+fi
+;;
+esac
+
+exit 0
